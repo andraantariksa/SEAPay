e33ef863e0026f2e8535ccff4daa26601ddfb313
Dipta <dipta004@gmail.com>
2019-07-28 15:23:45 +0700

    testing ui

---
+++ target/web/classes/main/META-INF/resources/webjars/seapay/1.0-SNAPSHOT/precache-manifest.9c9de84d31fb135cdf41f92069d57e23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687c73b2dd82ab02b4854149de6ee101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e645e1b5010bbee2f744",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "e645e1b5010bbee2f744",
+    "url": "/static/js/main.011d427b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,135 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown, Container, Col, Row, Button, Form } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link, NavLink, Switch } from "react-router-dom";
 
-function App() {
+function Home() {
   return (
-    <div className="App">
+    <Row>
+      <Col xs={6} md={4}>
+      </Col>
+      <Col xs={6} md={4}>
+        <h1>Welcome to SEA Pay</h1>
+      </Col>
+      <Col xs={6} md={4}>
+      </Col>
+    </Row>
+  )
+}
+
+function Signin() {
+  return (<Container><Row>
+    <Col xs={6} md={4}>
+    </Col>
+    <Col xs={6} md={4}>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicChecbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Col>
+    <Col xs={6} md={4}>
+    </Col>
+  </Row></Container>)
+}
+
+function Signup() {
+  return (<Container><Row>
+    <Col xs={6} md={4}>
+    </Col>
+    <Col xs={6} md={4}>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" pattern="[A-Z]{3,4}" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicChecbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Col>
+    <Col xs={6} md={4}>
+    </Col>
+  </Row></Container>)
+}
+
+function NotFound({ location }) {
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
     </div>
   );
 }
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+    <Router>
+      <header style={{ marginBottom: 50, }}>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <Link to="/" className="navbar-brand">
+              <img
+                alt=""
+                src="https://react-bootstrap.github.io/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />
+            {' SEA Pay'}
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            <Nav>
+              <NavLink to="/signin" className="nav-link">Signin</NavLink>
+              <NavLink to="/signup" className="nav-link">Signup</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    </Router>
+    )
+  }
+}
