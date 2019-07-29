a88e5abd3eeaf2df0e8024c23c85c2c558cbc0df
Andra Antariksa Prihadi <andra.antariksa@gmail.com>
2019-07-29 22:21:19 +0700

    Update

--- target/scala-2.13/routes/main/controllers/routes.java
+++ target/scala-2.13/routes/main/controllers/routes.java
@@ -1,6 +1,6 @@
 // @GENERATOR:play-routes-compiler
-// @SOURCE:/home/hsjsjsj009/Desktop/seapay/conf/routes
-// @DATE:Sun Jul 28 15:15:24 WIB 2019
+// @SOURCE:/home/andraantariksa/Projects/SEAPay/conf/routes
+// @DATE:Mon Jul 29 20:24:44 WIB 2019
 
 package controllers;
 
